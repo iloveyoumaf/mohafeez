143712c03e3bb4390be7fda11581d53cbf271fa6
iloveyoumaf <mohamadrezaoraei78@gmail.com>
2016-12-21 22:46:19 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "get",
     "broadcast",
     "download_media",
-    "invite",
+    "invite",Sudo _users = {221896057},--sudo_users
     "all",
     "leave_ban",
     "admin",
